6a3919c9a32f6e43e8057cd2db3862075d612fad
JHenrique <henryque.pc.game@gmail.com>
2025-05-05 23:10:57 -0300

    preparando projeto autenticacao

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/',function () {
-    echo "Welcome to Laravel Studies";
+    try {
+        DB::connection()->getPdo();
+        echo "connectado la base de datos";
+
+    } catch (\Throwable $th) {
+        echo "error: " . $th->getMessage();
+    }
 });
