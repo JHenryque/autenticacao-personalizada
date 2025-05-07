ffcf004c8ae27afd6c23a0dbfd9103706bd97c3b
JHenrique <henryque.pc.game@gmail.com>
2025-05-07 19:20:28 -0300

    AuthController e Formulário de Login

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,14 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
-
-Route::get('/',function () {
-    try {
-        DB::connection()->getPdo();
-        echo "connectado la base de datos";
-
-    } catch (\Throwable $th) {
-        echo "error: " . $th->getMessage();
-    }
-});
-
-Route::get('/test', function(){
-    echo 'teste';
-})->name('test')->middleware('auth');
-
-Route::get('/home', [MainController::class, 'home'])->name('home');
-
-Route::get('/login', function () {
-    echo 'formulario de ingreso';
-})->name('login');
+use App\Http\Controllers\AuthController;
 
 Route::middleware('guest')->group(function () {
-    Route::get('/register', function () {
-        echo 'formulario de registro';
-    })->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 });
 
-Route::get('/registro',function(){ echo 'formulario de registro';})->name('registro')->middleware('guest');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [MainController::class, 'home'])->name('home');
+});
+
